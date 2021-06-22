3d8763f9128587ccb1e179622ce346bb788558f8
panda1909 <umerfarooqb97@gmail.com>
2021-06-22 19:13:31 +0500

    all

--- GreenJeeva/settings.py
+++ GreenJeeva/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -138,3 +139,6 @@ X_FRAME_OPTIONS = "SAMEORIGIN"
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
