fac2a323fb45548c76ba0e91955576844c68289f
Muhammad Umair <Umairmuhammad433@gmail.com>
2021-07-24 19:43:46 +0500

    complete frontend

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home
-
+from .views import home, products, detail
 
 app_name = 'core'
 
 urlpatterns = [
-    path('', home , name='home')
+    path('', home, name='home'),
+    path('detail', detail, name='detail'),
+    path('products', products, name='home'),
 ]
--- core/views.py
+++ core/views.py
@@ -4,4 +4,12 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
+    return render(request, 'landing.html')
+
+
+def products(request):
+    return render(request, 'core/products.html')
+
+
+def detail(request):
     return render(request, 'core/product_details.html')
